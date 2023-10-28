5846c7eff17a8668c8e81efe70872965f80751c3
francosparn <francosparn@gmail.com>
2023-10-28 19:44:22 -0300

    update about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,9 @@ function About(){
                     <i className="fa-solid fa-circle-info fa-2xl"></i>
                 </div>
                 <h2 className="fw-bold mb-5">Sobre mi</h2>
-                <p>Mi nombre es Franco, soy un entusiasta de la tecnología y principalmente del desarrollo de software. En el año 2019 me gradué de la carrera de <span>Analista de Sistemas</span> y desde entonces me he mantenido en constante capacitación. Soy un desarrollador back-end especializado en <span>Django</span> y <span>Django REST Framework</span>. También me gusta crear aplicaciones dinámicas, visualmente agradables y livianas en <span>JavaScript</span> y <span>React</span>. Si mi perfil es de tu interés, puedes <a href="mailto:francosparn@gmail.com" className="link">contactarme</a> para que podamos trabajar juntos.</p>
+                <p>¡Hola! Soy Franco, un entusiasta de la tecnología y principalmente del desarrollo de software. En el año 2019 me gradué de la Carrera de <span>Analista de Sistemas de Computación</span> y desde entonces, me encuentro en constante formación.</p>
+                <p>Mi experiencia se centra en el desarrollo back-end, con un enfoque especial en el ecosistema de <span>Python</span>. Me especializo en tecnologías como <span>Django</span> y <span>Django REST Framework</span>, las cuales me han permitido crear aplicaciones web y APIs robustas y escalables.</p>
+                <p>Si mi perfil es de tu interés, ¡puedes <a href="mailto:francosparn@gmail.com" className="link">contactarme</a> para que podamos trabajar juntos en proyectos innovadores!</p>
             </div>
         </section>
     );
